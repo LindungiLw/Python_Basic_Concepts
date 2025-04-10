c28bce4b00281a872a5788de4cfabc8337ed8d78
RahmaLindungiLaowo <rahma23@jiu.ac>
2025-04-10 19:55:59 +0700

    String fill is add

---
+++ Basic_Python/variable/String_.py
@@ -0,0 +1,24 @@
+JIU = "Jakarta Internasional Univesity"
+print(JIU)
+print("JIU")
+
+score = 90
+print(score * 2)
+print(90)
+
+price_product = 20000
+quantity = 5
+print(price_product * quantity)
+print(price_product * 8)
+
+name = "Rahma Lindungi Laowo"
+major = "Information System"
+score = 94
+print(name, major)
+print("My name is", name, "\nMajor:", major, "\nScore:", score)
+
+x, y, z = 90, 95, 86
+print("Math score:", x)
+print("Statistic score:", y)
+print("Programming score:", z)
+print("All score:", x, ",", y, ",", z)
