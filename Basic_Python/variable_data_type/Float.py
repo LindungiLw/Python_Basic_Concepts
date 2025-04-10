1620a2ea12a7aaadb09db4eef6debed1aec6d45a
RahmaLindungiLaowo <rahma23@jiu.ac>
2025-04-10 20:30:28 +0700

    "float and integer"

---
+++ Basic_Python/variable_data_type/Float.py
@@ -0,0 +1,14 @@
+print(4.321 / 3 + 10)
+print(4.321 / (3 + 10))
+
+print(abs(-3.2))
+
+print(max(12, 26.5, 3.5))
+
+price = input("Enter the price: ")
+print(price * 4)
+
+price = float(price)
+print(price)
+
+print(price * 4)
---
+++ Basic_Python/variable_data_type/Integer.py
@@ -0,0 +1,56 @@
+print(3 + 7)
+print(3 * 7)
+print(5 / 2)
+print(4 / 2)
+print(2 * 3 + 1)
+print((3 + 1) * 3)
+print(3 - 2 + 1)
+print(3)
+
+print(2**3)
+print(2**4)
+print(14//3)
+print(14%3)
+
+print(abs(-4) )
+print(abs(4) )
+print(min (6, -2))
+print(max(6, -2))
+print(min(2, -4, 6, -2))
+
+helen = 95
+print (helen)
+print(helen - 3)
+
+lindungi = helen
+print (lindungi)
+lindungi = 95
+temp = lindungi
+lindungi = temp
+print()
+
+
+putri = 95
+putri -= 3
+print(putri)
+
+putri += 8
+print(putri)
+
+putri *= 2
+print(putri)
+
+putri /= 3
+print(putri)
+
+putri = 14
+putri //= 4
+print(putri)
+
+price = input ("Enter the price: ")
+print(price)
+print(price * 4)
+
+price = int(price)
+print(price)
+print(price * 4)
